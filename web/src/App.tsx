f7c9d49453315a7c170ba36e5da681e7f54aca7c
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-05-04 17:34:43 +0200

    Added content main: landing-page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,7 +9,21 @@ function App() {
   return (
     <div id="page-landing">
       <div className="content-wrapper">
-        <img src={logoImg} alt=""/>
+        <img src={logoImg} alt="Logo-happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Badajoz</strong>
+          <span>Extremadura</span>
+        </div>
+
+        <a href="" className="enter-app">
+          
+        </a>
       </div>
     </div>
   );
