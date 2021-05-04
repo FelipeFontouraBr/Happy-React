c45c3b7be47c7093596920abb0b963203ad05469
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-05-04 17:30:37 +0200

    Added logo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 import './styles/global.css';
 import './styles/pages/landing.css';
 
+import logoImg from './images/logo.svg';
+
 function App() {
   return (
     <div id="page-landing">
-      <h1>Welcome Fontureb</h1>
+      <div className="content-wrapper">
+        <img src={logoImg} alt=""/>
+      </div>
     </div>
   );
 }
