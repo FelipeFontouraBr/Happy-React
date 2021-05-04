e682a91fa79cff372d6804498801bdfba0d36d26
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-05-04 19:00:10 +0200

    Setting routes: home

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,1 +1,17 @@
-import {} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Route path="/" component={Landing}/>
+
+        </BrowserRouter>
+
+    );
+}
+
+export default Routes;
