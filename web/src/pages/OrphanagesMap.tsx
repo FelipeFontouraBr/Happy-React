1936a3de35a06b52f0477bf2e42963c075904a1b
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-05-05 13:32:26 +0200

    Added info and Link

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,6 +1,30 @@
+import mapMarkerImg from '../images/map-marker.svg'
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi'
+
 function OrphanagesMap() {
     return (
-        <h1>Hello Fontureb</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Elije un orfanato</h2>
+                    <p>Muchos ninõs esperán vuestras visitas </p>
+                </header>
+
+                <footer>
+                    <strong>Badajoz</strong>
+                    <span>Extremadura</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="fff"/>
+            </Link>
+        </div>
     )
 }
 
