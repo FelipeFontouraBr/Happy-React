560232e0ec0c175a7380f2b146835fe32be384cc
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-05-11 17:37:08 +0200

    View details about orphanages

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -13,6 +13,17 @@ export default {
         return response.json(orphanages);
     },
 
+    //Mostrar detalhes do orfanato
+    async show(request: Request, response: Response) {
+
+        const { id } = request.params; //pegando o ID
+
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanage = await orphanagesRepository.findOneOrFail(id)
+
+        return response.json(orphanage);
+    },
 
     async create(request: Request, response: Response) {
         const {
