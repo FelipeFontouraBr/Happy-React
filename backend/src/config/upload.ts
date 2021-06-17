da7341ef1a6ad4e61a771f34666b2d4f371b04ff
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-06-17 13:21:43 +0200

    Setting file Upload

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -5,7 +5,7 @@ export default {
     storage: multer.diskStorage({
         destination: path.join(__dirname, '..', '..', 'uploads'), //volto duas pastas e entro na pasta upload
         filename: (request, file, cb) => {
-            const fileName = `${Date.now()}-${file.originalname}`; //Tempo do arquivo e nome do arquivo
+            const fileName = `${Date.now()}-${file.originalname}`; //Representação da data e nome do arquivo
 
             cb(null, fileName);
         }
